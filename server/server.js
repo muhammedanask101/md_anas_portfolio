5684d0a6038f1e307690fb7631a1da4c0efec7c9
Muhammed Anas K <muhammedanask247@gmail.com>
2025-11-07 23:29:51 +0530

    test push

--- server/server.js
+++ server/server.js
@@ -8,11 +8,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 const app = express();
 
-app.use(cors({
-  origin: ["https://md-anas-portfolio.vercel.app"],
-  methods: ["POST","GET"],
-  credentials: true
-}))
+app.use(cors())
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
